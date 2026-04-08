a4950aff5626891b078736b3134ee8babe4def86
sanskarchaurasia8 <chaurasiasanskar8@gmail.com>
2026-04-08 15:14:07 +0530

    completed

--- Backend/src/ai/model.ai.ts
+++ Backend/src/ai/model.ai.ts
@@ -8,6 +8,7 @@ import config from "../config/config.js";
 export const geminiModel = new ChatGoogle({
     model: "gemini-flash-latest",
     apiKey: config.GOOGLE_API_KEY,
+    maxRetries: 5,
 })
 
 export const mistrAIModel = new ChatMistralAI({
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 import runGraph from './ai/graph.ai.js';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // Adjust this to your frontend URL
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 
 app.get('/', async (req, res) => {
 
     const result = await runGraph("Write a code for factorial function in js");
     res.json(result);
 });
+
+app.post('/invoke', async (req, res) => {
+
+    const { input } = req.body;
+    const result = await runGraph(input);
+
+    res.status(200).json({
+        message: "Graph executed successfully",
+        success: true,
+         result
+    })
+})
 
 export default app;
